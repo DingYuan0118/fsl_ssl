67d109254eeeadcec636ce6b1c73a098b28cbfd1
Yuan <804547696@qq.com>
2020-12-25 11:52:35 +0800

    pass (to complete)

--- filelists/recognition36_crop/write_RCG_filelists.py
+++ filelists/recognition36_crop/write_RCG_filelists.py
@@ -30,96 +30,96 @@ for class_name in class_names_unique:
 print(len(classfile_list_all))
 
 
-for mode in ["car", "plane", "all"]:
-    if mode == "all":
-        for dataset in dataset_list:
-            dataset += "_all"
-            file_list = []
-            label_list = []
-            for i, classfile_list in enumerate(classfile_list_all):
-                file_list = file_list + classfile_list
-                label_list = label_list + np.repeat(i, len(classfile_list)).tolist()
+# for mode in ["car", "plane", "all"]:
+#     if mode == "all":
+#         for dataset in dataset_list:
+#             dataset += "_all"
+#             file_list = []
+#             label_list = []
+#             for i, classfile_list in enumerate(classfile_list_all):
+#                 file_list = file_list + classfile_list
+#                 label_list = label_list + np.repeat(i, len(classfile_list)).tolist()
 
-            fo = open(savedir + dataset + ".json", "w")
-            fo.write('{"label_names": [')
-            fo.writelines(['"%s",' % item  for item in class_names_unique])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write('],')
+#             fo = open(savedir + dataset + ".json", "w")
+#             fo.write('{"label_names": [')
+#             fo.writelines(['"%s",' % item  for item in class_names_unique])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write('],')
 
-            fo.write('"image_names": [')
-            fo.writelines(['"%s",' % item  for item in file_list])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write('],')
+#             fo.write('"image_names": [')
+#             fo.writelines(['"%s",' % item  for item in file_list])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write('],')
 
-            fo.write('"image_labels": [')
-            fo.writelines(['%d,' % item  for item in label_list])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write(']}')
+#             fo.write('"image_labels": [')
+#             fo.writelines(['%d,' % item  for item in label_list])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write(']}')
 
-            fo.close()
-            print("%s -OK" %dataset)
+#             fo.close()
+#             print("%s -OK" %dataset)
 
-    if mode == "car":
-        for dataset in dataset_list:
-            dataset += "_car"
-            file_list = []
-            label_list = []
-            for i, classfile_list in enumerate(classfile_list_all[:19]):
-                file_list = file_list + classfile_list
-                label_list = label_list + np.repeat(i, len(classfile_list)).tolist()
+#     if mode == "car":
+#         for dataset in dataset_list:
+#             dataset += "_car"
+#             file_list = []
+#             label_list = []
+#             for i, classfile_list in enumerate(classfile_list_all[:19]):
+#                 file_list = file_list + classfile_list
+#                 label_list = label_list + np.repeat(i, len(classfile_list)).tolist()
 
-            fo = open(savedir + dataset + ".json", "w")
-            fo.write('{"label_names": [')
-            fo.writelines(['"%s",' % item  for item in class_names_unique])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write('],')
+#             fo = open(savedir + dataset + ".json", "w")
+#             fo.write('{"label_names": [')
+#             fo.writelines(['"%s",' % item  for item in class_names_unique])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write('],')
 
-            fo.write('"image_names": [')
-            fo.writelines(['"%s",' % item  for item in file_list])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write('],')
+#             fo.write('"image_names": [')
+#             fo.writelines(['"%s",' % item  for item in file_list])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write('],')
 
-            fo.write('"image_labels": [')
-            fo.writelines(['%d,' % item  for item in label_list])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write(']}')
+#             fo.write('"image_labels": [')
+#             fo.writelines(['%d,' % item  for item in label_list])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write(']}')
 
-            fo.close()
-            print("%s -OK" %dataset)
+#             fo.close()
+#             print("%s -OK" %dataset)
 
-    if mode == "plane":
-        for dataset in dataset_list:
-            dataset += "_plane"
-            file_list = []
-            label_list = []
-            for i, classfile_list in enumerate(classfile_list_all[19:]):
-                file_list = file_list + classfile_list
-                label_list = label_list + (np.repeat(i, len(classfile_list)) + 19).tolist()
+#     if mode == "plane":
+#         for dataset in dataset_list:
+#             dataset += "_plane"
+#             file_list = []
+#             label_list = []
+#             for i, classfile_list in enumerate(classfile_list_all[19:]):
+#                 file_list = file_list + classfile_list
+#                 label_list = label_list + (np.repeat(i, len(classfile_list)) + 19).tolist()
 
-            fo = open(savedir + dataset + ".json", "w")
-            fo.write('{"label_names": [')
-            fo.writelines(['"%s",' % item  for item in class_names_unique])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write('],')
+#             fo = open(savedir + dataset + ".json", "w")
+#             fo.write('{"label_names": [')
+#             fo.writelines(['"%s",' % item  for item in class_names_unique])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write('],')
 
-            fo.write('"image_names": [')
-            fo.writelines(['"%s",' % item  for item in file_list])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write('],')
+#             fo.write('"image_names": [')
+#             fo.writelines(['"%s",' % item  for item in file_list])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write('],')
 
-            fo.write('"image_labels": [')
-            fo.writelines(['%d,' % item  for item in label_list])
-            fo.seek(0, os.SEEK_END) 
-            fo.seek(fo.tell()-1, os.SEEK_SET)
-            fo.write(']}')
+#             fo.write('"image_labels": [')
+#             fo.writelines(['%d,' % item  for item in label_list])
+#             fo.seek(0, os.SEEK_END) 
+#             fo.seek(fo.tell()-1, os.SEEK_SET)
+#             fo.write(']}')
 
-            fo.close()
-            print("%s -OK" %dataset)
+#             fo.close()
+#             print("%s -OK" %dataset)
---
+++ filelists/ship5class/write_ship_filelists.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+from os import listdir
+from os.path import isfile, isdir, join
+from pathlib import Path
+
+
+
+cwd = os.getcwd() 
+train_data_path = 'filelists/ship5class/5class'
+test_data_path = 'filelists/ship5class/5class_test'
+savedir = 'filelists/ship5class/'
+dataset_list = ["train", "test", "novel"]
+train_class_list = listdir(train_data_path)
+test_class_list = listdir(test_data_path)
+assert train_class_list == test_class_list, "train test classes don't match"
+
+class_names = []
+for class_name in train_class_list:
+    class_names.append(class_name)
+class_names_unique = np.unique(class_names).tolist()
+
+train_classfile_list = []
+test_classfile_list = []
+total_classfile_list = []
+
+for class_name in class_names_unique:
+    train_image_name = []
+    test_image_name = []
+    train_classfile_path = Path(join(train_data_path, class_name)).as_posix()
+    test_classfile_path = Path(join(test_data_path, class_name)).as_posix()
+    for train_file_name in listdir(train_classfile_path):
+        if train_file_name[-4:] == ".jpg":
+            train_image_name.append(Path(join(train_classfile_path, train_file_name)).as_posix())
+    for test_file_name in listdir(test_classfile_path):
+        if test_file_name[-4:] == ".jpg":
+            test_image_name.append(Path(join(test_classfile_path, test_file_name)).as_posix())
+    
+    train_classfile_list.append(train_image_name)
+    test_classfile_list.append(test_image_name)
+        
+print(len(train_classfile_list))
+print(len(test_classfile_list))
+
+
+
+for dataset in dataset_list:
+    file_list = []
+    label_list = []
+    for i, classfile_list in enumerate(classfile_list_all):
+        file_list = file_list + classfile_list
+        label_list = label_list + np.repeat(i, len(classfile_list)).tolist()
+
+    fo = open(savedir + dataset + ".json", "w")
+    fo.write('{"label_names": [')
+    fo.writelines(['"%s",' % item  for item in class_names_unique])
+    fo.seek(0, os.SEEK_END) 
+    fo.seek(fo.tell()-1, os.SEEK_SET)
+    fo.write('],')
+
+    fo.write('"image_names": [')
+    fo.writelines(['"%s",' % item  for item in file_list])
+    fo.seek(0, os.SEEK_END) 
+    fo.seek(fo.tell()-1, os.SEEK_SET)
+    fo.write('],')
+
+    fo.write('"image_labels": [')
+    fo.writelines(['%d,' % item  for item in label_list])
+    fo.seek(0, os.SEEK_END) 
+    fo.seek(fo.tell()-1, os.SEEK_SET)
+    fo.write(']}')
+
+        fo.close()
+        print("%s -OK" %dataset)
